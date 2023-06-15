34407a4435dd1c1943e125b0b1f12befd48b2e09
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-15 22:07:33 +0530

    export on function itself

--- src/calc.js
+++ src/calc.js
@@ -2,5 +2,9 @@ function sum(n1, n2) {
   return n1 + n2;
 }
 //By default all the function are private in JS  so by using export we make it public
-//Module 
-export { sum };
+//Module
+
+function subtraction(n1, n2) {
+  return n1 - n2;
+}
+export { sum, subtraction };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-import { sum } from "./calc.js";
-let output = sum(10, 20);
+import { sum, subtraction } from "./calc.js";
+let output = sum(101, 42);
+let result = subtraction(500, 357);
 console.log("Sum :" + output);
+console.log("Difference :" + result);
